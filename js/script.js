7b8c36ef0802274fa4504794e0c3e57e27b0c224
YekaiLiu <55725880+YekaiLiu@users.noreply.github.com>
2021-08-30 21:49:17 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // getting all required elements
 
 const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector(".input");
+const inputBox = searchWrapper.querySelector("input");
 const suggBox = searchWrapper.querySelector(".autocom-box");
 const icon = searchWrapper.querySelector(".icon");
 let linkTag = searchWrapper.querySelector("a");
